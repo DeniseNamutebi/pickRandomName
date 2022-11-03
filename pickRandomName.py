f2b2a1ca7c3b4a692c0abf8d5a2fc68caffc8f78
DeniseNamutebi <denisenamutebi@gmail.com>
2022-11-03 18:01:25 +0000

    adding delays

--- pickRandomName.py
+++ pickRandomName.py
@@ -1,16 +1,30 @@
 import random
+import time
+import sys
 
 namesList = []
-print("Enter your the names of each person")
-while True: 
-    line = input()
-    if line: 
-        namesList.append(line)
-    else: 
-        break
-print("The names to choose from are")
-for x in namesList: print(x)
-randomIndex = random.randint(0,len(namesList)-1)
-print("The person picked is " + namesList[randomIndex])
+def createNamesList():
+    print("Enter your the names of each person")
+    while True: 
+        line = input()
+        if line: 
+            namesList.append(line)
+        else: 
+            break
+    print("The names to choose from are")
+    for x in namesList: print(x)
+
+def randomName(names):
+    randomIndex = random.randint(0,len(names)-1)
+    print("The name picked is ")
+    anticipation = "..."
+    for x in anticipation:
+        print (x, end=" ", flush=True),
+        #sys.stdout.flush()
+        time.sleep(1)
+    return print(names[randomIndex])
 
 
+createNamesList()
+print("\n")
+randomName(namesList)
